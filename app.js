c4e9695a8a4eecf20912a7b5cdd8556ab0a6a64e
AVANTICAPE\gustavo.lozano <gustavo.lozano@avantica.net>
2018-05-03 12:17:41 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/', function (req, res) {
+  res.send('Got a POST request')
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
