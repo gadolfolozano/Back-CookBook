c04bbe41d24e36165df3f9e598f85197f472f3c5
AVANTICAPE\gustavo.lozano <gustavo.lozano@avantica.net>
2018-05-03 16:47:44 -0500

    add mongoose dummy test

--- app.js
+++ app.js
@@ -1,6 +1,54 @@
 const express = require('express')
 const app = express()
 
+//connecting database
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/cats');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log('DB connected successfully!');
+
+  var kittySchema = mongoose.Schema({
+    name: String
+  });
+
+  kittySchema.methods.speak = function () {
+  var greeting = this.name
+      ? "Meow name is " + this.name
+      : "I don't have a name";
+    console.log(greeting);
+  }
+
+  var Kitten = mongoose.model('Kitten', kittySchema);
+
+  var fluffy = new Kitten({ name: 'fluffy' });
+  fluffy.speak(); // "Meow name is fluffy"
+
+  var silence = new Kitten({ name: 'Silence' });
+  console.log(silence.name); // 'Silence'
+
+  fluffy.save(function (err, fluffy) {
+    if (err) return console.error(err);
+    fluffy.speak();
+  });
+
+  silence.save(function (err, fluffy) {
+    if (err) return console.error(err);
+    fluffy.speak();
+  });
+
+  Kitten.find(function (err, kittens) {
+    if (err) return console.error(err);
+    console.log(kittens);
+  })
+  
+});
+
+
+
+//REST
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.post('/', function (req, res) {
